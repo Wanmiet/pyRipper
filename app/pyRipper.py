051c20c7b9839a64e9c91b943d6ee5b9ff64db26
Mieter <adryenestewart@aol.com>
2018-07-12 14:24:37 -0400

    Added options; get links or download images automatically.

--- app/pyRipper.py
+++ app/pyRipper.py
@@ -1,13 +1,37 @@
 from bs4 import BeautifulSoup
-import requests 
+import requests, random
+import urllib.request
 
-user_input = input("Enter your website: ")
-url = user_input
-r = requests.get(url)
+print("Welcome to pyRipper by Wanmiet.")
 
-html = r.text
-soup = BeautifulSoup(html, 'html.parser')
-body = soup.body
 
-for url in soup.find_all('img'):
-	print(url.get('src'))
+def get_links():
+	user_input = input("Enter your website: ")
+	url = user_input
+	r = requests.get(url)
+
+	html = r.text
+	soup = BeautifulSoup(html, 'html.parser')
+	body = soup.body
+
+	for url in soup.find_all('img'):
+		print(url.get('src'))
+
+
+def dl_images():
+	url = input("Enter image link: ")
+	name = random.randrange(1, 100000)
+	full_name = str(name) + ".jpg"
+	dld = urllib.request.urlretrieve(url, full_name)
+
+print('\n')
+while True:
+	u_i = input("1 - Download image links 2 - Download images 3 - Exit. ")
+	if u_i == "1":
+		get_links()
+	elif u_i == "2":
+		dl_images()
+	elif u_i == "3":
+		break
+	else:
+		print("Command not recognized. Try again.")
