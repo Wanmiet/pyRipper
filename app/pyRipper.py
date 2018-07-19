f0e48552d8e7e98f2933ac8887eaf9f62288e148
Mieter <adryenestewart@aol.com>
2018-07-19 16:18:37 -0400

    Now saves links to txt files.

--- app/pyRipper.py
+++ app/pyRipper.py
@@ -1,37 +1,47 @@
 from bs4 import BeautifulSoup
-import requests, random
+import requests
+import random
 import urllib.request
+import time
+import shutil
+import os
 
 print("Welcome to pyRipper by Wanmiet.")
-
+print('\n')
 
 def get_links():
-	user_input = input("Enter your website: ")
-	url = user_input
-	r = requests.get(url)
+    user_input = input("Enter your website: ")
+    url = user_input
+    r = requests.get(url)
+    filename = input("Enter name of txt file: ")
 
-	html = r.text
-	soup = BeautifulSoup(html, 'html.parser')
-	body = soup.body
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+    #body = soup.body
 
-	for url in soup.find_all('img'):
-		print(url.get('src'))
-
+    with open(filename + ".txt", 'w') as out:
+        for url in soup.find_all('img'):
+            out.write(str(url.get('src')))
+    print('\n')
+    print("Saved " + filename + ".txt" + " in directory.")
 
 def dl_images():
-	url = input("Enter image link: ")
-	name = random.randrange(1, 100000)
-	full_name = str(name) + ".jpg"
-	dld = urllib.request.urlretrieve(url, full_name)
+    while True:
+        url = input("Enter image link: ")
+        name = random.randrange(1, 100000)
+        full_name = str(name) + ".jpg"
+        urllib.request.urlretrieve(url, full_name)
 
-print('\n')
 while True:
-	u_i = input("1 - Download image links 2 - Download images 3 - Exit. ")
-	if u_i == "1":
-		get_links()
-	elif u_i == "2":
-		dl_images()
-	elif u_i == "3":
-		break
-	else:
-		print("Command not recognized. Try again.")
+    u_i = input(
+        "1 - Download image links 2 - Download images 3 - Exit. ")
+    if u_i == "1":
+        get_links()
+    elif u_i == "2":
+        dl_images()
+    elif u_i == "3":
+        print("Goodbye.")
+        time.sleep(3)
+        break
+    else:
+        print("Command not recognized. Try again.")
